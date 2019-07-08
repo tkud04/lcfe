92e7a0f22b2c425cdfb02519a204ab449c194495
ANonymous <postmaster.richmama@gmail.com>
2019-07-08 18:58:56 +0100

    cookie consent

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,13 @@
  @include('head')
  @include('header')
-        <!--------- Cookie consent-------------->
+
+<!-- notifications -->
+<section class="contact py-5">
+	<div class="container wow fadeInUp">
+		<h2 class="heading mb-lg-5 mb-4">Register</h2>
+		<div class="row contact-grids w3-agile-grid">
+			<div class="col-md-12 contact-grid1 w3-agile-grid">
+			        <!--------- Cookie consent-------------->
         	@include('cookie-consent')
         
         <!--------- Session notifications-------------->
@@ -28,7 +35,12 @@
                           @include('input-errors', ['errors'=>$errors])
                      @endif 
     
-	
+            </div>
+		</div>
+		
+	</div>
+</section>
+<!-- //send message-->
 @yield("banner")
 @yield("content")         
 
