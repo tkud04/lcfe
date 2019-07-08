363179d5cde87a2296cc7c5eeeb0fb6f39c79916
ANonymous <postmaster.richmama@gmail.com>
2019-07-08 19:22:29 +0100

    cookie consent

---
+++ resources/views/ad.blade.php
@@ -0,0 +1,17 @@
+<section class="quotes py-5 text-center">
+
+	<div class="container py-md-3">
+
+		<div class="quotes-info agile-info-quotes wow fadeInLeftBig">
+
+			<h3 class="mb-3">Get started  with us</h3>
+
+			<p>We promise hassle free transactions and a wonderful customer experience</p>
+
+			<a class="bt mt-4 text-capitalize start" href="{{url('register')}}" role="button"> Get Started</a>
+
+		</div>
+
+	</div>
+
+</section>
