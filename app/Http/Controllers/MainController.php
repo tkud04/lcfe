60014cd18303d6be0a8e1361f5b5b37aaf919510
ANonymous <postmaster.richmama@gmail.com>
2019-07-19 17:00:44 +0100

    download file

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -276,12 +276,27 @@ class MainController extends Controller {
 	 */
 	public function getDownload()
     {
-		$file= public_path(). "/download/account.pdf";
-        $headers = [
-              'Content-Type' => 'application/pdf',
-           ];
-
-        return response()->download($file, 'filename.pdf', $headers);
+		$fullPath = public_path('account.pdf');
+		
+        if($fullPath) {
+            $fsize = filesize($fullPath);
+            $path_parts = pathinfo($fullPath);
+            $ext = strtolower($path_parts["extension"]);
+            switch ($ext) {
+                case "pdf":
+                header("Content-Disposition: attachment; filename=\"".$path_parts["basename"]."\""); // use 'attachment' to force a download
+                header("Content-type: application/pdf"); // add here more headers for diff. extensions
+                break;
+                default;
+                header("Content-type: application/octet-stream");
+                header("Content-Disposition: filename=\"".$path_parts["basename"]."\"");
+            }
+            if($fsize) {//checking if file size exist
+              header("Content-length: $fsize");
+            }
+			
+			readfile($fullPath);
+		}  
     }
 
     /**
