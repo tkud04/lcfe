de13ed1e1d6e2ab22e7f095b60a51d31ca1f9d31
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-07-26 18:32:11 +0100

    Cookie consent

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,12 @@
 (function ($) {
     'use strict';
     console.log("main script online");
-	
+    
+	let x = getCookie('lcfe_gdpr');
+    if (x) {
+      $('#cookieConsent').hide();
+   }
+
     var $window = $(window);
     
 	$("#register-online-1").hide();
@@ -108,6 +113,31 @@ $("#farmer-card").click(function(e){
 
 
 })(jQuery);
+
+
+/********** Helper functions **********/
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
 
 function setPaymentAction(type){
 	let paymentURL = "";
