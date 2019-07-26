d2e04945899883089a2a87a0e1d1aec8192c8d15
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-07-26 18:02:28 +0100

    Get stock chunks

--- app/Helpers/Contracts/HelperContract.php
+++ app/Helpers/Contracts/HelperContract.php
@@ -6,6 +6,7 @@ Interface HelperContract
         public function sendEmailSMTP($data,$view,$type="view");
         public function createUser($data);
         public function getStocks();
+        public function getStockChunks();
 		public function checkout($user, $data, $type);
         public function payWithKloudPay($user, $data);
         public function payWithPayStack($user, $payStackResponse);
